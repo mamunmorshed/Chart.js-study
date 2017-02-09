89510b09e42fd4f61da27c3ec4de8750ff334255
Mamun Morshed <imroony@gmail.com>
2017-02-09 17:32:21 +0600

    practiced default bar chart

--- script/script.js
+++ script/script.js
@@ -1,3 +1,42 @@
 /**
  * Created by Mamun Morshed on 2/9/2017.
  */
+
+
+var chartone = document.querySelectorAll('.chartone')[0];
+new Chart(chartone, {
+    type: 'bar',
+    data: {
+        labels: ["Red", "Blue", "Green", "Yellow", "Orange", "Cyan"],
+        datasets:[{
+            label: '# of votes',
+            data: [12, 19, 2, 5, 3, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            bordeWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+        legend: {
+            labels:{
+                fontStyle: "italic"
+            }
+        },
+        scales: {
+            yAxes:[{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+
+});
+
