8368b086a038739e2495df8177165124b1eb9d78
Mamun Morshed <imroony@gmail.com>
2017-02-09 18:41:41 +0600

    practiced radar chart

--- script/script.js
+++ script/script.js
@@ -155,3 +155,52 @@ new Chart(chartfour, {
 
 });
 
+var chartfive = document.querySelectorAll('.chartfive')[0];
+new Chart(chartfive, {
+    type: 'radar',
+    data: {
+        labels: ["Red", "Blue", "Green", "Yellow", "Orange", "Cyan"],
+        datasets:[{
+            label: '# of 1st round votes',
+            data: [12, 19, 22, 25, 13, 30],
+            fill: true,
+            pointRadius: 0,
+            backgroundColor: 'rgba(153, 102, 255, 0.2)',
+            borderColor: 'rgba(153, 102, 255, 0)',
+            borderWidth: 1
+        },
+        {
+            label: '# of 2nd round votes',
+            data: [32, 9, 20, 15, 30, 13],
+            fill: true,
+            pointRadius: 0,
+            backgroundColor: 'rgba(255, 206, 86, 0.2)',
+            borderColor: 'rgba(255, 206, 86, 0.0)',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        title:{
+            display: true,
+            text: 'my chart work',
+            position: 'bottom'
+        },
+        legend: {
+            display: false,
+            position:'bottom'
+        },
+        hover: {
+            mode: 'nearest'
+        },
+        responsive: true,
+        scales: {
+            yAxes:[{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+
+});
+
