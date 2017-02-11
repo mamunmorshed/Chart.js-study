f3e9c544059017097fdc42ed5249640c37bb5a3e
Mamun Morshed <imroony@gmail.com>
2017-02-12 00:52:42 +0600

    practiced doughnut chart.

--- script/script.js
+++ script/script.js
@@ -418,3 +418,99 @@ new Chart(chartnine, {
 
 });
 
+
+var chartten = document.querySelectorAll('.chartten')[0];
+new Chart(chartten, {
+    type: 'doughnut',
+    data: {
+        labels: ["Red", "Blue", "Green", "Yellow", "Orange", "Cyan"],
+        datasets:[
+        {
+            label: 'My Data-set',
+            data: [32, 9, 20, 15, 30, 13],
+            fill: true,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.5)',
+                'rgba(54, 162, 235, 0.5)',
+                'rgba(255, 206, 86, 0.5)',
+                'rgba(75, 192, 192, 0.5)',
+                'rgba(153, 102, 255, 0.5)',
+                'rgba(255, 159, 64, 0.5)'
+            ],
+            hoverBackgroundColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        title:{
+            display: false,
+            text: 'my chart work',
+            position: 'bottom'
+        },
+        legend: {
+            display: true,
+            position:'top'
+        },
+        hover: {
+            mode: 'nearest'
+        },
+        responsive: true
+    }
+
+});
+
+
+var charteleven = document.querySelectorAll('.charteleven')[0];
+new Chart(charteleven, {
+    type: 'doughnut',
+    data: {
+        labels: ["Red", "Blue", "Green", "Yellow", "Orange", "Cyan"],
+        datasets:[
+        {
+            label: 'My Data-set',
+            data: [32, 9, 20, 15, 30, 13],
+            fill: true,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.5)',
+                'rgba(54, 162, 235, 0.5)',
+                'rgba(255, 206, 86, 0.5)',
+                'rgba(75, 192, 192, 0.5)',
+                'rgba(153, 102, 255, 0.5)',
+                'rgba(255, 159, 64, 0.5)'
+            ],
+            hoverBackgroundColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        title:{
+            display: false,
+            text: 'my chart work',
+            position: 'bottom'
+        },
+        legend: {
+            display: true,
+            position:'top'
+        },
+        hover: {
+            mode: 'nearest'
+        },
+        responsive: true
+    }
+
+});
+
